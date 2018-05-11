84b174120c7a84d8605faa479fd5eb6e1c719085
LEMSantos <lucas_m-santos@hotmail.com>
2018-05-11 14:32:22 -0300

    Definição da busca genética

--- GeneticSearch.py
+++ GeneticSearch.py
@@ -15,19 +15,20 @@ class GeneticSearch:
     def getPopulation(self):
         return self.population.copy()
 
-    def mutation(self):
-        for i in range(len(self.population)):
-            network_aux = self.population[i].getNetwork()
+    def mutation(self,new_population):
+        new_population_aux = new_population[:]
+        for i in range(len(new_population_aux)):
+            network_aux = new_population_aux[i].getNetwork()
             for layer in network_aux:
                 for neuron in layer:
-                    for i in range(len(neuron)):
+                    for k in range(len(neuron)):
                         if random.random() <= self.mutationRate:
-                            neuron[i] += random.gauss(0,1)
-            self.population[i].setNetwork(network_aux)
+                            neuron[k] += random.gauss(0,1)
+            new_population_aux[i].setNetwork(network_aux)
+        return new_population_aux.copy()
 
     def crossOver(self, p1, p2):
         alpha = random.random()
-        print("alpha =",alpha)
         f1 = nn.NeuralNetwork([self.neuralConfiguration])
         f2 = nn.NeuralNetwork([self.neuralConfiguration])
         f1.setNetwork(p1.getNetwork())
@@ -49,24 +50,36 @@ class GeneticSearch:
 
     def reproduction(self, parents_list):
         new_population = []
-        for i in range(self.populationSize/2):
+        for i in range(1,len(parents_list),2):
+            children = self.crossOver(parents_list[i-1],parents_list[i])
             for j in range(2):
-                children = self.crossOver(parents_list[],)
-                new_population.append()
-        
+                new_population.append(children[j])
+        new_population = self.mutation(new_population)
+        return new_population.copy()
+
+    def selection(self,fitness):
+        F = sum(fitness)
+        probs = []
+        acum_probs = []
+        index_pop = []
+        parents_list = []
+        for i in range(len(fitness)):
+            probs.append(fitness[i]/F)
+            if i > 0:
+                acum_probs.append(probs[i] + acum_probs[i-1])
+            else:
+                acum_probs.append(probs[i])
+        for i in range(len(fitness)):
+            index = 0
+            num = random.random()
+            for j in range(len(acum_probs)):
+                if acum_probs[j] >= num:
+                    index = j
+                    break
+            parents_list.append(self.population[index])
+        return parents_list.copy()
 
     def evolution(self, fitness):
         parents_list = self.selection(fitness)
-        self.population = self.reproduction(parents_list).copy()
+        self.population = self.reproduction(parents_list)
 
-def main():
-    solução = GeneticSearch(1,0.2,4,[2,1])
-    população = solução.getPopulation()
-    for i in range(2):
-        print(população[i].getNetwork())
-    new_son = solução.crossOver(população[0],população[1])
-    print("\n")
-    for i in range(2):
-        print(new_son[i].getNetwork())
-    
-main()
